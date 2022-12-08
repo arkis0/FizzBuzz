2f888cec8fa594de007cf3a8e09449e98f460965
arkis0 <kontotak163@gmail.com>
2022-12-08 19:03:52 +0100

    Added: try, catch, game description

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -11,28 +11,39 @@ namespace FizzBuzz
         static void Main(string[] args)
         {
             // Opis gry
+            Console.WriteLine("----------FizzBuzz----------\n");
+            Console.WriteLine("Zasady gry:\n- Jeżeli liczba użytkownika będzie podzielna przez 3 bez reszty, to powinien zostać zwrócony wynik „Fizz”.\n-Jeżeli liczba użytkownika będzie podzielna przez 5 bez reszty, to powinien zostać zwrócony wynik „Buzz”.\n-Jeżeli liczba użytkownika będzie podzielna przez 3 i przez 5 jednocześnie bez reszty, to powinien zostać\n zwrócony wynik „FizzBuzz”.\n-Jeżeli liczba użytkownika nie będzie podzielna przez 3 ani przez 5 bez reszty, to wtedy zwracasz podaną liczbę.\n");
+
             Console.WriteLine("Podaj liczbę: ");
             var score = FizzBuzz();
             Console.WriteLine(score);
+            Console.ReadLine();
         }
         private static string FizzBuzz()
         {
-            //dodać try, catch(debilo-odporność)<3
-            int number = int.Parse(Console.ReadLine());
-            if (number % 3 == 0 && number % 5 == 0 && number % 10 != 0)
+            try
             {
-                return "FizzBuzz";
-            }
-            else if(number % 3 == 0 && number % 5 != 0)
-            {
-                return "Fizz";
-            }
-            else if(number % 5 == 0 && number % 3 != 0)
-            {
-               return "Buzz";
-            }
+                int number = int.Parse(Console.ReadLine());
+                if (number % 3 == 0 && number % 5 == 0 && number % 10 != 0)
+                {
+                    return "FizzBuzz";
+                }
+                else if (number % 3 == 0 && number % 5 != 0)
+                {
+                    return "Fizz";
+                }
+                else if (number % 5 == 0 && number % 3 != 0)
+                {
+                    return "Buzz";
+                }
 
-            return $"Liczba {number} nie jest podzielna ani przez 3, ani przez 5.";
+                return $"Liczba {number} nie jest podzielna ani przez 3, ani przez 5.";
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return " ";
         }
     }
 }
